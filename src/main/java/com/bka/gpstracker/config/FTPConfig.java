dabe7382bd9a4a2b9ab56f348fefe462fd3ad65d
ghoulkh <ghoul.handle@gmail.com>
2023-07-14 00:03:47 +0700

    modified config ftp server

--- src/main/java/com/bka/gpstracker/config/FTPConfig.java
+++ src/main/java/com/bka/gpstracker/config/FTPConfig.java
@@ -3,29 +3,33 @@ package com.bka.gpstracker.config;
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.net.PrintCommandListener;
 import org.apache.commons.net.ftp.FTPClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import sun.net.ftp.FtpClient;
 
 import java.io.PrintWriter;
 
 @Configuration
 @Log4j2
 public class FTPConfig {
+    @Value("${ftp.hostname}")
+    private String hostname;
+    @Value("${ftp.username}")
+    private String username;
+    @Value("${ftp.password}")
+    private String password;
 
     @Bean
     public FTPClient ftpClient() {
         FTPClient ftp = new FTPClient();
 
-        ftp.addProtocolCommandListener(new PrintCommandListener(new PrintWriter(System.out)));
-
         try {
-            ftp.connect("gpstracker.somee.com", 21);
+            ftp.connect(this.hostname, 21);
             ftp.enterLocalPassiveMode();
             int reply = ftp.getReplyCode();
             System.out.println(reply);
 
-            ftp.login("gpstracker", "Daotak1310.");
+            ftp.login(this.username, this.password);
         } catch (Exception e) {
             log.error(e.getMessage(), e);
         }
